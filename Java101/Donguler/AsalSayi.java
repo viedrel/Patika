034956d4f689ac542975111a0ffeb68fd785721e
Emre Altuntaş <110238425+viedrel@users.noreply.github.com>
2023-03-22 18:44:16 +0300

    AsalSayi eklendi

---
+++ Java101/Donguler/AsalSayi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class AsalSayi {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            boolean asalMi = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+        if (asalMi) {
+            System.out.println(i+" asaldır.");
+            }
+        }
+    }
+}
